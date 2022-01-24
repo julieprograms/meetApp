e41c52b62b3b1cffcd32395f15ab8302dcb2c523
julieprograms <junge.julie@web.de>
2022-01-24 06:28:58 +0100

    added api access

--- src/CitySearch.js
+++ src/CitySearch.js
@@ -3,7 +3,8 @@ import React, { Component } from 'react';
 export class CitySearch extends Component {
     state = {
         query: '',
-        suggestions: []
+        suggestions: [],
+        showSuggestions: undefined
       };
 
     handleInputChanged = (event) => {
@@ -19,7 +20,8 @@ export class CitySearch extends Component {
 
     handleItemClicked = (suggestion) => {
         this.setState({
-            query: suggestion
+            query: suggestion,
+            showSuggestions: false
         });
 
         this.props.updateEvents(suggestion);
@@ -31,8 +33,10 @@ export class CitySearch extends Component {
         <input type="text" className="city" 
         value={this.state.query} 
         onChange={this.handleInputChanged}
+        onFocus={() => { this.setState({ showSuggestions: true }) }}
         />
         <ul className="suggestions">
+            style={this.state.showSuggestions ? {}: { display: 'none'}}
             {this.state.suggestions.map((suggestion) => (
                 <li 
                 key={suggestion}
