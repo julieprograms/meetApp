836416d3c29aa25997a6479f5b3b68038642eb27
julieprograms <junge.julie@web.de>
2022-01-26 01:39:30 +0100

    added end-to-end test and acceptance test

--- src/Event.js
+++ src/Event.js
@@ -35,7 +35,7 @@ export class Event extends Component {
             <p className='description'>{event.description}</p>
            </div>
             )}
-                <button className={`${collapsed ? "show" : "hide"}-details`}
+                <button id='details-btn' className={`${collapsed ? "show" : "hide"}-details`}
         onClick={this.handleClick}>{collapsed ? "Show Details" : "Hide-Details"}</button>
         
     </div> 
